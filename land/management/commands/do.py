74fd593cef740bfc5ae47a96d56f783f6b0ef3a6
Eugen Ciur <eugen@django-lessons.com>
2020-05-08 16:16:30 +0200

    a mode advanced cmd line interface

--- land/management/commands/do.py
+++ land/management/commands/do.py
@@ -1,4 +1,13 @@
 from django.core.management.base import BaseCommand
+from land.models import Plan, Product
+
+
+CREATE = 'create'
+UPDATE = 'update'
+LIST = 'list'
+DELETE = 'delete'
+PRODUCT = 'product'
+PLAN = 'plan'
 
 
 class Command(BaseCommand):
@@ -9,27 +18,63 @@ class Command(BaseCommand):
             'operation',
             help="Operation to perform",
             choices=(
-                'create',
-                'list',
-                'delete',
-                'update'
+                CREATE,
+                LIST,
+                DELETE,
+                UPDATE
             )
         )
         parser.add_argument(
             'model',
             help="Model to operate onto",
             choices=(
-                'product',
-                'plan',
+                PRODUCT,
+                PLAN,
             )
         )
+        parser.add_argument(
+            '--title',
+        )
+        parser.add_argument(
+            '--description',
+        )
+
+    def create(self, model, title, description):
+        if model == PRODUCT:
+            Product.objects.create(
+                title=title,
+                description=description
+            )
+        if model == PLAN:
+            Plan.objects.create(
+                title=title,
+                description=description
+            )
+
+    def list(self, model):
+        if model == PRODUCT:
+            for p in Product.objects.all():
+                self.stdout.write(
+                    f"Product {p.title} {p.description}"
+                )
+
+        if model == PLAN:
+            for p in Plan.objects.all():
+                self.stdout.write(
+                    f"Plan {p.title} {p.description}"
+                )
 
     def handle(self, *args, **options):
         op = options.get('operation')
         model = options.get('model')
-        self.stdout.write(
-            f"Doing {op}... on {model}"
-        )
+        title = options.get('title')
+        description = options.get('description')
+
+        if op == CREATE:
+            self.create(model, title, description)
+        if op == LIST:
+            self.list(model)
+
         self.stdout.write(
             self.style.SUCCESS("Done! Success")
         )
